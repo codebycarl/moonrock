1f2ea2880ea24733c2d007439a8ed08301e5cf96
/carlysle <38358895+codebycarl@users.noreply.github.com>
2018-09-26 14:45:39 -0400

    Generate Post model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,35 @@
 class PostsController < ApplicationController
+	before_action :set_post, except: [:index, :new, :create]
+
   def index
+  	@posts = Post.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_post
+  	@post = Post.find(params[:id])
+  end
+
+  def post_params
+  	params.require(:post).permit(:title, :description)
   end
 end
