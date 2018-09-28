ea0b6e450a736ebcd7f34c313fe25d0453890041
/carlysle <38358895+codebycarl@users.noreply.github.com>
2018-09-27 23:23:10 -0400

    Add photo upload to form and photo display in show view

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,7 @@ class Post < ApplicationRecord
 
 	# Validates the description field is less than 300 characters
 	validates :description, length: {maximum: 300, too_long: 'cannot be more than %{count} characters long'}
+
+	# Active record macro to associate uploads with posts
+	has_one_attached :photo
 end
